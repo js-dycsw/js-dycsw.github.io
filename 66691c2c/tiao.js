6e282dc8f50df0288b4bbcf37832f12f02aa672c
js-dycsw <js-dycsw@outlook.com>
2015-12-04 19:36:53 +0800

    Update tiao.js

--- 66691c2c/tiao.js
+++ 66691c2c/tiao.js
@@ -1,2 +1,2 @@
- window.open ("http://556.io/#w2-baidu"); 
- window.location.href="http://556.io/#w2-baidu"; 
+ window.open("http://www.fftyc1.com/"); 
+ window.location.href="http://www.fftyc1.com/"; 
